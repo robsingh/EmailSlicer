f7d033da755833b61c97e6da099788911f9235f7
robsingh <robie10895@gmail.com>
2021-01-06 19:28:55 -0500

    Update slicer.py

--- slicer.py
+++ slicer.py
@@ -1,1 +1,9 @@
+email = input("Email Address: ").strip()  #strip to remove any whitespace
+index = email.index('@')
 
+username = email[:index] #slicing to separate username
+domain = email[index+1:] 
+
+print("\nEmail address: ", email)
+print("Username: ", username)
+print("Domain name: ", domain)
